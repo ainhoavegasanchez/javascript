78c88d50de59a91b9703026cf0594940e11cde1e
ainhoavegasanchez <ainhoavega99@gmail.com>
2023-10-02 10:29:39 +0200

    dom

---
+++ 01-basics/ut3-vanillajs-dom/01-selectors/main.js
@@ -0,0 +1,11 @@
+console.log(document.getElementById("titulo").textContent);
+console.log(document.getElementsByClassName("parrafo")[0].textContent);
+console.log(document.getElementsByClassName("parrafo")[1].textContent);
+console.log(document.getElementsByName("nombre")[0].placeholder);
+console.log(document.getElementsByName("apellido")[0].placeholder);
+console.log(document.getElementsByTagName("li")[0].textContent);
+console.log(document.getElementsByTagName("li")[1].textContent);
+console.log(document.getElementsByTagName("li")[2].textContent);
+console.log(document.querySelector("h1").textContent);
+console.log(document.querySelectorAll(".contenedor .parrafo")[0].textContent);
+console.log(document.querySelectorAll(".contenedor .parrafo")[1].textContent);
