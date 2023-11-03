30d56669eba3fc290cf9c2f1ac1170a93e96d72f
ainhoavegasanchez <ainhoavega99@gmail.com>
2023-11-03 09:36:04 +0100

    computacion

---
+++ 01-basics/ut4/poo-5/main.js
@@ -0,0 +1,49 @@
+import { Price } from "./classes/Price.js";
+import { Review } from "./classes/Review.js";
+import { Product } from "./classes/Product.js";
+
+let precio = new Price("USD", "399.00");
+
+let reviews = new Review(4.6, 73);
+
+let producto = new Product(
+  "New Apple iPhone SE (64GB, (Product) RED) [Locked] + Cricket Wireless Plan",
+  "https://www.amazon.com/dp/B087623TMW",
+  "https://m.media-amazon.com/images/I/810DvHOZ9nL._AC_UL320_.jpg",
+  "335.80",
+  precio,
+  reviews
+);
+
+let clavesProducto = Object.keys(producto);
+let valorProducto = Object.values(reviews);
+
+
+let lista = document.createElement("ul");
+let div = document.createElement("div");
+div.className = "container";
+
+
+Object.entries(producto).forEach(([clave, valor]) => {
+  let li = document.createElement("li");
+
+  if (clave == "price") {
+    li.textContent = clave +"= " +`${Object.values(precio)[0]} ${Object.values(precio)[1]}`;
+    lista.append(li);
+
+  } else if (clave === "review") {
+    Object.entries(valor).forEach(([key, value]) => {
+      let lis = document.createElement("li");
+      lis.textContent = key + "= " + value;
+      lista.append(lis);
+    });
+  } else {
+    li.textContent = clave + "= " + valor; 
+    lista.append(li);
+  }
+
+ 
+});
+
+div.appendChild(lista);
+document.body.appendChild(div);
